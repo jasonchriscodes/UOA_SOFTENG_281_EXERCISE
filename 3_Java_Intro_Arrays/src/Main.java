52b21e8821dd64db580ec6ffebc37603d3c7cb6f
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-08 20:41:15 +1300

    Initial code copied from previous exercise

--- 3_Java_Intro_Arrays/src/Main.java
+++ 3_Java_Intro_Arrays/src/Main.java
@@ -1,4 +1,20 @@
 
 public class Main {
-
+	
+	public static void main(String[] args) {
+		
+		int[] numbers = { 2, 4, 3, 5, 6 };
+		
+		int sum = 0;
+		
+		if (sum > 10) {
+			System.out.println("over");
+		} else if (sum < 0) {
+			System.out.println("under");
+		} else {
+			System.out.println("Total = " + sum);
+		}
+		
+	}
+	
 }
