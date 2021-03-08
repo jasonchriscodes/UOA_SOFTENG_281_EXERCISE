c30c1f830f34e30e9146285945a81bba3afdefb2
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-08 22:58:25 +1300

    Commenting

--- 3_Java_Intro_Arrays/src/Main.java
+++ 3_Java_Intro_Arrays/src/Main.java
@@ -1,11 +1,53 @@
+//// Array 1st Activity
+//public class Main {
+//	
+//	public static void sumOfArray() {
+//	
+//		int[] numbers = {8, 5, 1, 2, 11}; // over
+////		int[] numbers = {-8, 5, 1, 2, -11}; // under
+////		int[] numbers = {2, 5, 1, 2, 1}; // sum = 11	
+//		int sum = 0;
+//
+//// Choosing between index-based for-loop or for each elements inside numbers style for-loop
+//		
+//		// index-based for-loop... 
+//		// still need this style if you need to know the "index" 
+////		for ( int index = 0 ; index < numbers.length ; index++ ) {
+////			int element = numbers[index];
+////			sum += element;	
+////			System.out.println("Element at index " + index + " = " + element);
+////		}
+//		
+//		// for each elements inside numbers style for-loop (nice and elegant when you have collections/arrays)
+//		for (int element : numbers) {
+//			sum += element;	// shortcut sum = sum + element
+//		}
+//		
+//		if (numbers.length == 0) { // .length is an attribute of an array
+//			System.out.println("empty");
+//		} else if (sum > 20) {
+//			System.out.println("over");
+//		} else if (sum < 5) {
+//			System.out.println("under");
+//		} else {
+//			System.out.println("Total = " + sum);
+//		}
+//	}
+//	
+//	public static void main(String[] args) {
+//		System.out.println("Array 1st Activity");
+//		sumOfArray();	
+//	}
+//}	
 
+// Array 2nd Activity
 public class Main {
 	
 	public static void sumOfArray(int[] numbers) {
-		
-//		int[] numbers = { 2, 3, 4, 34, 2, 34 , 3 };
-		
+	
 		int sum = 0;
+
+// Choosing between index-based for-loop or for each elements inside numbers style for-loop
 		
 		// index-based for-loop... 
 		// still need this style if you need to know the "index" 
@@ -15,12 +57,12 @@ public class Main {
 //			System.out.println("Element at index " + index + " = " + element);
 //		}
 		
-		// foreach style for-loop (nice and elegant when you have collections/arrays)
-		for ( int element : numbers ) {
-			sum += element;		 
+		// for each elements inside numbers style for-loop (nice and elegant when you have collections/arrays)
+		for (int element : numbers) {
+			sum += element;	// shortcut sum = sum + element
 		}
 		
-		if (numbers.length == 0) {
+		if (numbers.length == 0) { // .length is an attribute of an array
 			System.out.println("empty");
 		} else if (sum > 20) {
 			System.out.println("over");
@@ -32,24 +74,28 @@ public class Main {
 	}
 	
 	public static void main(String[] args) {
+		System.out.println("Array 2nd Activity");
 		
-//		System.out.println("Hello");
-//		
+//		// use variable
+//		int[] paramToSend = {3, 34, 566, 5,32}; // declare a variable of paramToSend of type in array
+//		sumOfArray(paramToSend);
+//		System.out.println("The first element in the array is: " + paramToSend[0]); // to print the first variable in the array
+		
+//		System.out.println("Hello");	// example use of variable to print
 //		String paramToSend = "Hello";
 //		System.out.println(paramToSend);
 		
-		
-		int[] paramToSend = { 3, 34, 566, 5,32};
-	    sumOfArray(paramToSend);
-//	    System.out.println("The first element in the array is: " + paramToSend[0]);
-	    
-	    sumOfArray(new int[] { 3, 34, 566, 5,32});
-	    
-//		sumOfArray(new int[]{});
-//	    sumOfArray(new int[]{23});
-//	    sumOfArray(new int[]{-5});
-//	    sumOfArray(new int[]{-3, -2, 1});
-//	    sumOfArray(new int[]{2, 4, 8, -2, 1});		
+		sumOfArray(new int[]{}); // u need to tell java this block is a new int array 
+		sumOfArray(new int[]{3});
+	    sumOfArray(new int[]{23});
+	    sumOfArray(new int[]{-5});
+	    sumOfArray(new int[]{-3, -2, 1});
+	    sumOfArray(new int[]{2, 4, 8, -2, 1});	
 	}
-	
 }
+
+		
+		
+  
+    
+		
