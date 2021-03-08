a810e0764499268a7f39b27fc4ab9b80b406fee0
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-08 15:47:00 +1300

    Completed with under and print sum

--- 2_Java_Intro/src/Main.java
+++ 2_Java_Intro/src/Main.java
@@ -2,13 +2,17 @@
 public class Main {
 	public static void main(String[] args) {
 
-		int first = 3;    
-		int second = 50;
+		int first = 23;    
+		int second = 5;
 
 		int sum = first + second;
 		
 		if (sum > 10) {
 			System.out.println("over");
+		} else if (sum < 0) {
+			System.out.println("under");
+		} else {
+			System.out.println("Total = " + sum);
 		}
 		
 	}
