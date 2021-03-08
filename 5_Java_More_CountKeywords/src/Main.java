31cac056f43ebf4a0f92a7489009e9538d302cfb
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-09 03:11:13 +1300

    Initial Code

--- 5_Java_More_CountKeywords/src/Main.java
+++ 5_Java_More_CountKeywords/src/Main.java
@@ -1,9 +1,38 @@
 
-public class Main {
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Main {
 
+	public static int countKeywords(String keywords) {
+		return -1;
 	}
 
+	public static void main(String[] args) {
+
+		// Test 1
+		String test1 = "";
+		int result = countKeywords(test1);
+		System.out.println("Test 1 result: " + result);   // expect 0
+
+		// Test 2
+		String test2 = "fruit; vegetables; drinks";
+		result = countKeywords(test2);
+		System.out.println("Test 2 result: " + result);   // expect 3
+
+		// Test 3
+		String test3 = "flat white; latte; cappuccino; cafe au lait; mocha";
+		result = countKeywords(test3);
+		System.out.println("Test 3 result: " + result);   // expect 5
+
+		// Test 4
+		String test4 = "oolong; ; herbal;  ;;    ; matcha";
+		result = countKeywords(test4);
+		System.out.println("Test 4 result: " + result);   // expect 3
+
+		// Test 5
+		String test5 = ";hoki;;";
+		result = countKeywords(test5);
+		System.out.println("Test 5 result: " + result);   // expect 1
+
+	}
 }
