6dee3ef96764f3762d7a34c440dce22aad55884b
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-08 15:40:03 +1300

    Added a new Log class for fun

--- 1. Welcome Java/src/Welcome.java
+++ 1. Welcome Java/src/Welcome.java
@@ -3,7 +3,10 @@ public class Welcome {
 
 	public static void main(String[] args) {
 		String name = "Active Classroom Programmer (ACP)";
-		System.out.println("Welcome to " + name);
+		
+		Log.printLine();
+		Log.print("Welcome to " + name);
+		Log.printLine();
 	}
 
 }
