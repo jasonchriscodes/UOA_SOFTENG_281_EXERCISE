997b1f99db95511c896709ec684e986053bf4bf9
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-09 01:20:21 +1300

    Checking for an empty ArrayList

--- 4_Java_More_LongestWord/src/Main.java
+++ 4_Java_More_LongestWord/src/Main.java
@@ -5,47 +5,46 @@ class Main {
 
 	public static String longestWord(ArrayList<String> words) {
 		// TODO complete the code here...
-
-		return null;
+		
+		if (words.size() == 0) {
+			return null;
+		}
+		
+		//...
+		
+		return "SOMETHING ELSE";
 	}
 
 	public static void main(String[] args) {
 		
 		// Test 1
-//		ArrayList<String> emptyList = new ArrayList();
-//		String result = longestWord(emptyList);
-//		System.out.println("Test 1 result: " + result);   // null
-//
-//		// Test 2
-//		ArrayList<String> listWithOneWord = new ArrayList();
-//		listWithOneWord.add("apple");
-//		result = longestWord(listWithOneWord);
-//		System.out.println("Test 2 result: " + result);   // apple
+		ArrayList<String> emptyList = new ArrayList();
+		String result = longestWord(emptyList);
+		System.out.println("Test 1 result: " + result);   // null
+
+		// Test 2
+		ArrayList<String> listWithOneWord = new ArrayList();
+		listWithOneWord.add("apple");
+		result = longestWord(listWithOneWord);
+		System.out.println("Test 2 result: " + result);   // apple
 
 		// Test 3
 		ArrayList<String> listWithManyWords = new ArrayList();
 		listWithManyWords.add("blue");
 		listWithManyWords.add("green");
 		listWithManyWords.add("red");
-		System.out.println(listWithManyWords.size());
-		
-		System.out.println(listWithManyWords.get(1));
-		
-		listWithManyWords.clear();
-		System.out.println(listWithManyWords.size());
-		
-//		result = longestWord(listWithManyWords);
-//		System.out.println("Test 3 result: " + result);   // green
+		result = longestWord(listWithManyWords);
+		System.out.println("Test 3 result: " + result);   // green
 
 		// Test 4
-//		ArrayList<String> listWithTies = new ArrayList();
-//		listWithTies.add("WhatsApp");
-//		listWithTies.add("Zoom");
-//		listWithTies.add("Facebook");
-//		listWithTies.add("WeChat");
-//		listWithTies.add("Skype");
-//		result = longestWord(listWithTies);
-//		System.out.println("Test 4 result: " + result);  // WhatsApp
+		ArrayList<String> listWithTies = new ArrayList();
+		listWithTies.add("WhatsApp");
+		listWithTies.add("Zoom");
+		listWithTies.add("Facebook");
+		listWithTies.add("WeChat");
+		listWithTies.add("Skype");
+		result = longestWord(listWithTies);
+		System.out.println("Test 4 result: " + result);  // WhatsApp
 
 	}
 }
