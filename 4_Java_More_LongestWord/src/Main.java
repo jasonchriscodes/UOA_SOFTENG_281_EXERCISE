a4e7e8f9ffeaecec7f291e5835cf89437fe57e56
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-09 01:18:49 +1300

    Mucking around with Strings

--- 4_Java_More_LongestWord/src/Main.java
+++ 4_Java_More_LongestWord/src/Main.java
@@ -1,9 +1,56 @@
 
-public class Main {
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+class Main {
 
+	public static String longestWord(ArrayList<String> words) {
+		// TODO complete the code here...
+
+		return null;
 	}
 
+	public static void main(String[] args) {
+		
+		String message = "Hello how are you?";
+		
+//		String substring(int beginIndex);
+		
+		String res = message.substring(3, 5);
+		System.out.println(res);
+		
+		if (true ) {
+			return;
+		}
+		
+		
+		// Test 1
+		ArrayList<String> emptyList = new ArrayList();
+		String result = longestWord(emptyList);
+		System.out.println("Test 1 result: " + result);   // null
+
+		// Test 2
+		ArrayList<String> listWithOneWord = new ArrayList();
+		listWithOneWord.add("apple");
+		result = longestWord(listWithOneWord);
+		System.out.println("Test 2 result: " + result);   // apple
+
+		// Test 3
+		ArrayList<String> listWithManyWords = new ArrayList();
+		listWithManyWords.add("blue");
+		listWithManyWords.add("green");
+		listWithManyWords.add("red");
+		result = longestWord(listWithManyWords);
+		System.out.println("Test 3 result: " + result);   // green
+
+		// Test 4
+		ArrayList<String> listWithTies = new ArrayList();
+		listWithTies.add("WhatsApp");
+		listWithTies.add("Zoom");
+		listWithTies.add("Facebook");
+		listWithTies.add("WeChat");
+		listWithTies.add("Skype");
+		result = longestWord(listWithTies);
+		System.out.println("Test 4 result: " + result);  // WhatsApp
+
+	}
 }
