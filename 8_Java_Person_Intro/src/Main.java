26f2c1fb981c553ebe8428de40f77c4954bc9962
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-14 00:28:50 +1300

    Initialising fields at their declaration

--- 8_Java_Person_Intro/src/Main.java
+++ 8_Java_Person_Intro/src/Main.java
@@ -1,15 +1,21 @@
 class Main {
 	public static void main(String[] args) {
-		// -- just one variable to represent Bob 
-		// Person bob = new Person ("Bob", "Jones", 18);
-		
+
+		// -- just one variable to represent Bob    
+//		Person bob = new Person("Bob", "Jones", 18);
+
 		// -- just one variable to represent Jenny
 		Person jenny = new Person("Jenny", "Jones", 20);
-		
+
 		Person nas = new Person();
 		nas.introduceSelf();
 		
-		// bob.introduceSelf();
-		// jenny.introduceSelf();
+		Person baby = new Person("Windsor");
+		baby.introduceSelf();
+		
+//		bob.introduceSelf();
+//
+		jenny.introduceSelf();
+
 	}
 }
--- 8_Java_Person_Intro/src/Person.java
+++ 8_Java_Person_Intro/src/Person.java
@@ -1,13 +1,13 @@
 
 public class Person {
-	private String firstName;
-	private String lastName;
-	private int age;
+	private String firstName = null;
+	private String lastName = null;
+	private int age = 123;
 	
 	public Person() {
 		firstName = "NoFirstName";
 		lastName = "NoLastName";
-		age = 1;
+//		age = 1;
 	}
 	
 	public Person(String ln) {
@@ -15,16 +15,17 @@ public class Person {
 		lastName = ln;
 		age = 0;
 	}
-	
+
 	public Person(String fn, String ln, int a) {
 		firstName = fn;
 		lastName = ln;
 		age = a;
 	}
-	
+
 	public void introduceSelf() {
-		System.out.println("Hello, my name is " + firstName +
+		System.out.println("Hello, my name is " + firstName + 
 				" and I am " + age + " years old.");
 	}
+
 }
 
