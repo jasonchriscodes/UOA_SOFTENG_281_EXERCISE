1efbfa7e65ce55c4c8f6c07e3ff6904a015ff782
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-13 16:27:05 +1300

    Initial code with one constructor

--- 8_Java_Person_Intro/src/Main.java
+++ 8_Java_Person_Intro/src/Main.java
@@ -2,7 +2,17 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated 
+		
+		// -- just one variable to represent Bob
+		Person bob = new Person("Bob", "Jones", 18);
+		
+		// -- just one variable to represent Jenny
+		Person jenny = new Person("Jenny", "Jones", 20);
+		
+		bob.introduceSelf();
+		
+		jenny.introduceSelf();
+				
 
 	}
 
--- 8_Java_Person_Intro/src/Person.java
+++ 8_Java_Person_Intro/src/Person.java
@@ -1,4 +1,17 @@
 
 public class Person {
-
+	private String firstName;
+	private String lastName;
+	private int age;
+	
+	public Person(String fn, String ln, int a) {
+		firstName = fn;
+		lastName = ln;
+		age = a;
+	}
+	
+	public void introduceSelf() {
+		System.out.println("Hello, my name is" + firstName +
+				" and I am " + age + " years old.");
+	}
 }
