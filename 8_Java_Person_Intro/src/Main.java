26cb7146831f5655c8bea386e06ba56d6da18d47
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-14 00:29:43 +1300

    Declaring another variable, sharing the same instance with different variables

--- 8_Java_Person_Intro/src/Main.java
+++ 8_Java_Person_Intro/src/Main.java
@@ -2,20 +2,26 @@ class Main {
 	public static void main(String[] args) {
 
 		// -- just one variable to represent Bob    
-//		Person bob = new Person("Bob", "Jones", 18);
+		Person bob = new Person("Bob", "Jones", 18);
 
 		// -- just one variable to represent Jenny
 		Person jenny = new Person("Jenny", "Jones", 20);
-
-		Person nas = new Person();
-		nas.introduceSelf();
 		
-		Person baby = new Person("Windsor");
-		baby.introduceSelf();
+		Person someoneElse = bob;
+		someoneElse.introduceSelf();
+		
+		someoneElse= jenny;
+		someoneElse.introduceSelf();
+		
+//		Person nas = new Person();
+//		nas.introduceSelf();
+//		
+//		Person baby = new Person("Windsor");
+//		baby.introduceSelf();
 		
 //		bob.introduceSelf();
 //
-		jenny.introduceSelf();
+//		jenny.introduceSelf();
 
 	}
 }
