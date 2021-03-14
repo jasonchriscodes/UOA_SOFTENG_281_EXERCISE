ad2cc93ba62b73b5d478d6230e768c722064e90a
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-03-14 14:56:53 +1300

    Commenting

--- 8_Java_Person_Intro/src/Main.java
+++ 8_Java_Person_Intro/src/Main.java
@@ -1,21 +1,30 @@
 class Main {
 	public static void main(String[] args) {
 
-		// -- just one variable to represent Bob    
-		Person bob = new Person("Bob", "Jones", 18);
-
+		// -- just one variable to represent Bob, instance / object  
+		Person bob = new Person("Bob", "Jones", 18); 
+		// new Person means they creating a new custom type (Object type) not a primitive types(int, char, bool, etc.)
+		// They are known as reference type variable, example bob and jenny (a memory that tell us the location of Person("Bob", "Jones", 18)
+		
 		// -- just one variable to represent Jenny
-		Person jenny = new Person("Jenny", "Jones", 20);
+		Person jenny = new Person("Jenny", "Jones", 20); 
 		
-		Person someoneElse = bob;
+//		Person someoneElse = null; // a null pointer exception because there is no address that program want to point to
+//		someoneElse.introduceSelf();
+		
+		Person someoneElse = bob; // having 2 variable (bob, someoneElse, pointing the same address)
 		someoneElse.introduceSelf();
 		
-		someoneElse= jenny;
+		someoneElse = jenny;
 		someoneElse.introduceSelf();
 		
-//		Person nas = new Person();
-//		nas.introduceSelf();
-//		
+//		Person nas = new Person(); // the compiler complains "The constructor Person() is undefined (does not exist)"
+		// impossible to create a person instance by specifying nothing as input, there is only one constructor define
+		// that means there is only one way to create an instance of the person by provide the parameter (String fn, String ln, int a)
+		// if u want to make another instance without that parameter u need to create another constructor
+		//		nas.introduceSelf(); goes to variable and it finds where it is in memory then it calls the introduceSelf method on their instance
+		//	result is default value Helo, my name is null and I am 0 years old. (did not set the default value)
+		
 //		Person baby = new Person("Windsor");
 //		baby.introduceSelf();
 		
--- 8_Java_Person_Intro/src/Person.java
+++ 8_Java_Person_Intro/src/Person.java
@@ -1,27 +1,33 @@
 
-public class Person {
-	private String firstName = null;
+public class Person { // define custom type (person class)
+	private String firstName = null; // set default value for field unless changed by a constructor
 	private String lastName = null;
 	private int age = 123;
 	
-	public Person() {
+	public Person() { // make default value with default constructor
 		firstName = "NoFirstName";
 		lastName = "NoLastName";
 //		age = 1;
 	}
-	
-	public Person(String ln) {
+
+//  Note/ if the constructor does not match the instance input, it will complain (order, number of input)
+	public Person(String ln) { // 1st constructor, we need make constructor with different type combination
+		// otherwise, the compiler will confuse which one should be used, 
+		// compiler will ignore ln word and just see the type
 		firstName = "NoFirstNameYet";
 		lastName = ln;
 		age = 0;
 	}
 
-	public Person(String fn, String ln, int a) {
-		firstName = fn;
+	public Person(String fn, String ln, int a) { // 2nd constructor of the person class, filling the field of each instance 
+		firstName = fn;	// each instance will have field firstName, lastName, age
 		lastName = ln;
 		age = a;
 	}
 
+//	public Person(String fn = firstName, String lastName, int 10) { we CANNOT initialised variable in parenthese in java
+//	}
+	
 	public void introduceSelf() {
 		System.out.println("Hello, my name is " + firstName + 
 				" and I am " + age + " years old.");
