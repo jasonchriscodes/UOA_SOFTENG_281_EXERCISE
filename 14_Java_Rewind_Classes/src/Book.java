3b4619230e89c8a2f8957e5aaa2af00f2b58e745
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-05 13:52:58 +1200

    Added getters for Book class

--- 14_Java_Rewind_Classes/src/Book.java
+++ 14_Java_Rewind_Classes/src/Book.java
@@ -9,5 +9,58 @@ public class Book {
 	private int id;
 
 	private static int nextID = 0;
+
+	public Book(String title, String author, int year, double costPrice, double sellPrice) {
+		this.title = title;
+		this.author = author;
+		this.year = year;
+		this.costPrice = costPrice;
+		this.sellPrice = sellPrice;
+		this.id = nextID;
+		nextID++;
+	}
+
+	
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return title;
+	}
+
+	/**
+	 * @return the author
+	 */
+	public String getAuthor() {
+		return author;
+	}
+
+	/**
+	 * @return the year
+	 */
+	public int getYear() {
+		return year;
+	}
+
+	/**
+	 * @return the costPrice
+	 */
+	public double getCostPrice() {
+		return costPrice;
+	}
+
+	/**
+	 * @return the sellPrice
+	 */
+	public double getSellPrice() {
+		return sellPrice;
+	}
+
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
 	
 }
--- 14_Java_Rewind_Classes/src/Main.java
+++ 14_Java_Rewind_Classes/src/Main.java
@@ -7,6 +7,13 @@ class Main {
     System.out.println("Starting balance for " + shop.getShopName() + " is: " + shop.getCashBalance()); 
     System.out.println();
     
+ // Create a few Book instances
+    Book hp1 = new Book("Harry Potter Philosopher Stone", "J. K. Rowling", 1997, 10.50, 29.95); // ID: 0
+    Book hp2a = new Book("Harry Potter Chamber Secrets", "J. K. Rowling", 1998, 11.50, 31.95);  // ID: 1
+    Book hp2b = new Book("Harry Potter Chamber Secrets", "J. K. Rowling", 1998, 9.50, 31.95);   // ID: 2
+    Book phil = new Book("Philosophy 101", "Paul Kleinman", 2013, 8.75, 15.99);                 // ID: 3
+    
+    
   }
 
 }
