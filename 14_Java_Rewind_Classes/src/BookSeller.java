c3ff702000fc40f73854d74c67554412d882792b
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-05 13:56:55 +1200

    Implement purchaseStock method for BookSeller

--- 14_Java_Rewind_Classes/src/BookSeller.java
+++ 14_Java_Rewind_Classes/src/BookSeller.java
@@ -1,8 +1,10 @@
+import java.util.ArrayList;
 
 public class BookSeller {
 	
 	private String name;
 	private double balance;
+	private ArrayList<Book> collection = new ArrayList<>();
 	
 	public BookSeller(String name, double balance) {
 		this.name = name;
@@ -15,6 +17,12 @@ public class BookSeller {
 
 	public double getCashBalance() {
 		return this.balance;
+	}
+	
+	public void purchaseStock(Book book) {
+		balance -= book.getCostPrice();
+		
+		collection.add(book);
 	}
   
 }
