4d52f3ccd46af46300e21b3f5e0f2126ed72df2f
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-05 13:51:46 +1200

    Added fields for Book class

--- 14_Java_Rewind_Classes/src/Book.java
+++ 14_Java_Rewind_Classes/src/Book.java
@@ -1,4 +1,13 @@
 
 public class Book {
-  
+
+	private String title;
+	private String author;
+	private int year;
+	private double costPrice;
+	private double sellPrice;
+	private int id;
+
+	private static int nextID = 0;
+	
 }
