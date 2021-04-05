828ad574b876e2576a99da84b72f08a1018b7803
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-05 13:58:35 +1200

    Testing purchaseBook works

--- 14_Java_Rewind_Classes/src/Main.java
+++ 14_Java_Rewind_Classes/src/Main.java
@@ -13,10 +13,19 @@ class Main {
     Book hp2b = new Book("Harry Potter Chamber Secrets", "J. K. Rowling", 1998, 9.50, 31.95);   // ID: 2
     Book phil = new Book("Philosophy 101", "Paul Kleinman", 2013, 8.75, 15.99);                 // ID: 3
 
-    System.out.println(hp1.toString());
-    System.out.println(hp2a.toString());
-    System.out.println(hp2b.toString());
-    System.out.println(phil.toString());
+    shop.purchaseStock(hp1);
+    System.out.println("Balance for " + shop.getShopName() + " after purchasing 1 book is: " + shop.getCashBalance());
+    shop.purchaseStock(hp2a);
+    System.out.println("Balance for " + shop.getShopName() + " after purchasing 2 books is: " + shop.getCashBalance());
+    shop.purchaseStock(hp2b);
+    System.out.println("Balance for " + shop.getShopName() + " after purchasing 3 books is: " + shop.getCashBalance());
+    shop.purchaseStock(phil);
+    System.out.println("Balance for " + shop.getShopName() + " after purchasing 4 books is: " + shop.getCashBalance()); 
+    
+//    System.out.println(hp1.toString());
+//    System.out.println(hp2a.toString());
+//    System.out.println(hp2b.toString());
+//    System.out.println(phil.toString());
   }
 
 }
