c98a5347d23133685f2a8f335920054827180d30
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-05 13:49:30 +1200

    Added BookSeller constructor

--- 14_Java_Rewind_Classes/src/BookSeller.java
+++ 14_Java_Rewind_Classes/src/BookSeller.java
@@ -1,4 +1,12 @@
 
 public class BookSeller {
+	
+	private String name;
+	private double balance;
+	
+	public BookSeller(String name, double balance) {
+		this.name = name;
+		this.balance = balance;
+	}
   
 }
--- 14_Java_Rewind_Classes/src/Main.java
+++ 14_Java_Rewind_Classes/src/Main.java
@@ -2,7 +2,7 @@
 class Main {
 
   public static void main(String[] args) {
-    BookSeller shop = new BookSeller(); 
+    BookSeller shop = new BookSeller("Books r Us", 100); 
 
     System.out.println("Initial code...");
   }
