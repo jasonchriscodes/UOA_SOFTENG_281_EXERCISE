9e514f33bdf456a2ea87887945dfd238e77a20e0
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-09 12:38:03 +1200

    Overriding toString() again, but for Book class this time

--- 16_Java_281_RewindItemSeller/src/Book.java
+++ 16_Java_281_RewindItemSeller/src/Book.java
@@ -11,5 +11,10 @@ public class Book extends Item {
 	public String getAuthor() {
 		return this.author;
 	}
+	
+	@Override
+	public String toString() {
+		return "Book " + super.toString() + " " + this.author;
+	}
 
 }
