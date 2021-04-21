259b4be9358c168fad4ec48376047b2e5c961bea
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-04-22 11:35:02 +1200

    Add initial code from website

--- 5_1_Java_Enum/src/Main.java
+++ 5_1_Java_Enum/src/Main.java
@@ -1,9 +1,30 @@
+class Main {
 
-public class Main {
+  // public enum Day {MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+  static final int MONDAY=0, TUESDAY=1, WEDNESDAY=2, THURSDAY=3, FRIDAY=4, SATURDAY=5, SUNDAY=6;
 
-	}
+  public static void main(String[] args) {
+    
+    int dayToCheck = MONDAY;
 
+    if (dayToCheck == MONDAY) {
+        System.out.println("MONDAY is a weekday!");
+    } else if (dayToCheck == TUESDAY) {
+        System.out.println("TUESDAY is a weekday!");
+    } else if (dayToCheck == WEDNESDAY) {
+        System.out.println("WEDNESDAY is a weekday!");
+    } else if (dayToCheck == THURSDAY) {
+        System.out.println("THURSDAY is a weekday!");
+    } else if (dayToCheck == FRIDAY) {
+        System.out.println("FRIDAY is a weekday!");
+    } else if (dayToCheck == MONDAY) {
+        System.out.println("SATURDAY is a weekday!");
+    } else if (dayToCheck == MONDAY) {
+        System.out.println("SUNDAY is a weekday!");
+    } else {
+        System.out.println("Uh oh! We have more than 7 possible days of the week!");
+    } 
+
+  }
 }
