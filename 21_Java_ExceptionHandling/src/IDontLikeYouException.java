a8c3f7fd48573c6ca225f83fe7317251d5933f3b
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-07-13 21:37:28 +1200

    Created our own unchecked exception

---
+++ 21_Java_ExceptionHandling/src/IDontLikeYouException.java
@@ -0,0 +1,9 @@
+
+public class IDontLikeYouException extends RuntimeException {
+	
+	public IDontLikeYouException(String message)  {
+		super(message);
+	}
+	
+	
+}	
--- 21_Java_ExceptionHandling/src/Main.java
+++ 21_Java_ExceptionHandling/src/Main.java
@@ -13,7 +13,10 @@ class Main {
 		System.out.println("inside bar before");
 		if (name.toLowerCase().equals("nasser")) {
 //			System.out.println("I don't like you Nasser, that's what I didn't come to your class today and instead studied for ENGSCI 211. I should have failed ENGSCI 211 to show how much I appreciate you.");
-			throw new IllegalArgumentException("I don't like Nasser");
+			throw new IDontLikeYouException("I don't like Nasser");
+		} else if (name.toLowerCase().equals("bob")) {
+//			System.out.println("I don't like you Nasser, that's what I didn't come to your class today and instead studied for ENGSCI 211. I should have failed ENGSCI 211 to show how much I appreciate you.");
+			throw new IDontLikeYouException("I don't like Bob");
 		}
 		System.out.println("Hello, " + name);
 		System.out.println("inside bar after");
@@ -22,10 +25,11 @@ class Main {
 	public static void main(String[] args) {
 		
 		try {
-			foo("August");
+//			foo("August");
+			foo("Bob");
 //			foo("NasSer");
-		} catch(IllegalArgumentException e) {
-			System.out.println("I caught it! I caught it!!");
+		} catch(IDontLikeYouException e) {
+			System.out.println("I caught it! I caught it: " + e.getMessage());
 		}
 		System.out.println("All good, program completes...");
 		
