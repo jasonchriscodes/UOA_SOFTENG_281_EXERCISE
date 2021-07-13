4acac620ade7a79582bfc9de13f795492ab845db
mrb34n135421234 <70122753+mrb34n135421234@users.noreply.github.com>
2021-07-13 20:21:13 +1200

    Refractor

--- 1. Welcome Java/src/Welcome.java
+++ 1. Welcome Java/src/Welcome.java
@@ -1,14 +1,7 @@
-import logger.Log;
-
 public class Welcome {
 
 	public static void main(String[] args) {
 		String name = "Active Classroom Programmer (ACP)";
-		
-		Log.printLine();
-		Log.print("Welcome to " + name);
-		Log.print("Enjoy!");
-		Log.printLine();
 	}
 
 }
--- 8_Java_Person_Intro/src/Person.java
+++ 8_Java_Person_Intro/src/Person.java
@@ -3,39 +3,38 @@ public class Person { // define custom type (person class)
 	private String firstName = null; // set default value for field unless changed by a constructor
 	private String lastName = null;
 	private int age = 123;
-	
+
 	public Person() { // make default value with default constructor
 		firstName = "NoFirstName";
 		lastName = "NoLastName";
-//		age = 1;
+		// age = 1;
 	}
 
-//  Note/ if the constructor does not match the instance input, it will complain (order, number of input)
+	// Note/ if the constructor does not match the instance input, it will complain
+	// (order, number of input)
 	public Person(String ln) { // 1st constructor, we need make constructor with different type combination
-		// otherwise, the compiler will confuse which one should be used, 
+		// otherwise, the compiler will confuse which one should be used,
 		// compiler will ignore ln word and just see the type
 		firstName = "NoFirstNameYet";
 		lastName = ln;
 		age = 0;
 	}
 
-	public Person(String fn, String ln, int a) { // 2nd constructor of the person class, filling the field of each instance 
-		firstName = fn;	// each instance will have field firstName, lastName, age
+	public Person(String fn, String ln, int a) { // 2nd constructor of the person class, filling the field of each instance
+		firstName = fn; // each instance will have field firstName, lastName, age
 		lastName = ln;
 		age = a;
 	}
 
-//	public Person(String fn = firstName, String lastName, int 10) { we CANNOT initialised variable in parenthese in java
-//	}
-	
+	// public Person(String fn = firstName, String lastName, int 10) { we CANNOT
+	// initialised variable in parenthese in java
+	// }
+
 	public void introduceSelf() {
-		System.out.println("Hello, my name is " + firstName + 
-				" and I am " + age + " years old.");
+		System.out.println("Hello, my name is " + firstName + " and I am " + age + " years old.");
 	}
-	
+
 	public void meet(Person other) {
-		sysout
 	}
 
 }
-
